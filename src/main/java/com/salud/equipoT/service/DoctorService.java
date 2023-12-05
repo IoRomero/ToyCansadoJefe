b85a9e56e8bd43d7d860079160fdd74228358073
“Jonatan <jonatanalassia@hotmail.com>
2023-12-05 19:32:33 -0300

    Actualizar DoctorService

--- src/main/java/com/salud/equipoT/service/DoctorService.java
+++ src/main/java/com/salud/equipoT/service/DoctorService.java
@@ -6,18 +6,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salud.equipoT.entidad.Doctor;
+import com.salud.equipoT.entidad.Turno;
 import com.salud.equipoT.repository.DoctorRepository;
+import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DoctorService {
-    @Autowired
-    private DoctorRepository doctorRepository;
-    
-     public List<Doctor> buscarDoctoresPorEspecializacion(Long id){
 
-        return doctorRepository.buscarDoctoresPorEspecializacion(id);
+  @Autowired
+  private DoctorRepository doctorRepository;
+
+  @Transactional
+  public Doctor crearDoctor(Long matricula, String nombre) {
+    Doctor doctor = new Doctor();
+
+    doctor.setMatricula(matricula);
+    doctor.setNombre(nombre);
+
+    doctorRepository.save(doctor);
+
+    return doctor;
+  }
+
+  @Transactional
+  public void modificarDoctor(Long doctorId) throws Exception {
+
+    Optional<Doctor> respuestaDoctor = doctorRepository.findById(doctorId);
+
+    if (respuestaDoctor.isPresent()) {
+      Doctor doctor = respuestaDoctor.get();
+
+      doctorRepository.save(doctor);
     }
-    public Doctor getOne(Long matricula){
-        return doctorRepository.getOne(matricula);
-    } 
+  }
+
+  public List<Doctor> buscarDoctoresPorEspecializacion(Long id) {
+
+    return doctorRepository.buscarDoctoresPorEspecializacion(id);
+  }
+
+  public Doctor getOne(Long matricula) {
+    return doctorRepository.getOne(matricula);
+  }
 }
